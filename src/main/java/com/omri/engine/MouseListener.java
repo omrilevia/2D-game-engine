b5216eb001589d0acef3cf2522f532f4f44c71f2
omrilevia <omrilevia@gmail.com>
2021-10-18 15:32:29 -0700

    screen coords to world coords

--- src/main/java/com/omri/engine/MouseListener.java
+++ src/main/java/com/omri/engine/MouseListener.java
@@ -1,5 +1,7 @@
 package com.omri.engine;
 import static org.lwjgl.glfw.GLFW.*;
+
+import org.joml.Vector4f;
 public class MouseListener {
 	private static MouseListener instance;
 	private double scrollX, scrollY;
@@ -59,6 +61,26 @@ public class MouseListener {
 	
 	public static float getY() {
 		return (float)get().yPos;
+	}
+	
+	public static float getOrthoX() {
+		float currentX = getX();
+		currentX = (currentX / (float)Window.getWidth()) * 2.0f - 1.0f;
+		
+		Vector4f tmp = new Vector4f(currentX, 0, 0, 1);
+		tmp.mul(Window.getScene().camera().getInverseProjection()).mul(Window.getScene().camera().getInverseView());
+		currentX = tmp.x;
+		return currentX;
+	}
+	
+	public static float getOrthoY() {
+		float currentY = getY();
+		currentY = (currentY / (float)Window.getWidth()) * 2.0f - 1.0f;
+		
+		Vector4f tmp = new Vector4f(0, currentY, 0, 1);
+		tmp.mul(Window.getScene().camera().getInverseProjection()).mul(Window.getScene().camera().getInverseView());
+		currentY = tmp.y;
+		return currentY;
 	}
 	
 	public static float getDx() {
